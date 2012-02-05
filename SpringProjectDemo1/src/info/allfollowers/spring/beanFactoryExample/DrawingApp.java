d3d390db96487903f4b4ca393aec8848f54dc0cf
abhijit gaikwad <gaikwad.abhijit@gmail.com>
2012-02-05 02:21:25 -0500

    XmlBeanFactory example

--- SpringProjectDemo1/src/info/allfollowers/spring/beanFactoryExample/DrawingApp.java
+++ SpringProjectDemo1/src/info/allfollowers/spring/beanFactoryExample/DrawingApp.java
@@ -3,12 +3,19 @@ package info.allfollowers.spring.beanFactoryExample;
 import org.springframework.beans.factory.BeanFactory;
 import org.springframework.beans.factory.xml.XmlBeanFactory;
 import org.springframework.core.io.FileSystemResource;
-
+/**
+ * 
+ * @author abhijit
+ * 
+ *   XmlBeanFactory  example
+ *   
+ *   this uses the spring_xmlBeanFactory.xml
+ */
 public class DrawingApp {
 	
 	public static void main(String[] args) {
 	
-		BeanFactory factory = new XmlBeanFactory(new FileSystemResource("spring.xml"));
+		BeanFactory factory = new XmlBeanFactory(new FileSystemResource("spring_xmlBeanFactory.xml"));
 		
 		Triangle triangle =  (Triangle) factory.getBean("triangle");
 		triangle.draw();
