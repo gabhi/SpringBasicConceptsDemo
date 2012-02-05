42515cd5ffa7b558d45a6e4dcd2de708cd98c130
abhijit gaikwad <gaikwad.abhijit@gmail.com>
2012-02-05 02:20:39 -0500

    Inner Bean Example

---
+++ SpringProjectDemo1/src/info/allfollowers/spring/InnerBeanExample/DrawingApp.java
@@ -0,0 +1,32 @@
+package info.allfollowers.spring.InnerBeanExample;
+
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.xml.XmlBeanFactory;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.core.io.FileSystemResource;
+/**
+ * 
+ * @author abhijit
+ * 
+ *   XmlBeanFactory  example
+ *   
+ *   this uses the conf file for ApplicationContextExample
+ *   
+ *   this example also shows how to set property for a bean in conf file and how to get and set 
+ *   bean values
+ *   
+ *   
+ *   refer oobjects in the xml file example
+ *   
+ */
+public class DrawingApp {
+	
+	public static void main(String[] args) {
+	
+ 		ApplicationContext appCon = new ClassPathXmlApplicationContext("spring_InnerBeanExample.xml");
+		Triangle triangle =  (Triangle) appCon.getBean("triangle");
+		triangle.draw();
+  	}
+
+}
