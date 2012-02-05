70c49801716c39f06a8acea400595bdc25db11ea
abhijit gaikwad <gaikwad.abhijit@gmail.com>
2012-02-05 02:52:59 -0500

    Constructor  Injection example

--- SpringProjectDemo1/src/info/allfollowers/spring/ConstructorInjectionExample/DrawingApp.java
+++ SpringProjectDemo1/src/info/allfollowers/spring/ConstructorInjectionExample/DrawingApp.java
@@ -9,12 +9,7 @@ import org.springframework.core.io.FileSystemResource;
  * 
  * @author abhijit
  * 
- *   XmlBeanFactory  example
- *   
- *   this uses the conf file for ApplicationContextExample
- *   
- *   this example also shows how to set property for a bean in conf file and how to get and set 
- *   bean values
+ 
  *   
  *   
  *   Constructor  Injection example
